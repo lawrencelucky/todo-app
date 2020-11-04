3bc2de0e8083a76c4535552b9c30e00137f9b292
lawrencelucky <lawrencelucky1999@gmail.com>
2020-11-04 23:57:29 +0100

    Fix: Fixed the clicking issue of the checkbox and also improved the styles.

--- src/components/DisplayTasks.js
+++ src/components/DisplayTasks.js
@@ -9,57 +9,66 @@ function DisplayTasks() {
       <p>There are no available tasks.</p>
 
       <div className='displayTask__tasks'>
-        <label class='displayTask__task'>
-          <div>
+        <div className='displayTask__task'>
+          <label class='displayTask__task--label'>
+            <input type='checkbox' />
+            <span className='displayTask__task--text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+              temporibus excepturi sunt eos quas molestias? Lorem ipsum dolor
+              sit amet, consectetur adipisicing elit. Adipisci totam dignissimos
+              est fugit optio eius earum ducimus eos ut assumenda!
+            </span>
+          </label>
+
+          <HighlightOffIcon />
+        </div>
+
+        <div className='displayTask__task'>
+          <label class='displayTask__task--label'>
+            <input type='checkbox' />
+            <span className='displayTask__task--text'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+              temporibus excepturi sunt eos quas molestias? Lorem ipsum dolor
+              sit amet, consectetur adipisicing elit. Adipisci totam dignissimos
+              est fugit optio eius earum ducimus eos ut assumenda!
+            </span>
+          </label>
+
+          <HighlightOffIcon />
+        </div>
+
+        <div className='displayTask__task'>
+          <label class='displayTask__task--label'>
             <input type='checkbox' />
             <span className='displayTask__task--text'>
               Pick children at school
             </span>
-          </div>
+          </label>
 
           <HighlightOffIcon />
-        </label>
+        </div>
 
-        <label class='displayTask__task'>
-          <div>
+        <div className='displayTask__task'>
+          <label class='displayTask__task--label'>
             <input type='checkbox' />
             <span className='displayTask__task--text'>
               Pick children at school
             </span>
-          </div>
+          </label>
 
           <HighlightOffIcon />
-        </label>
-        <label class='displayTask__task'>
-          <div>
+        </div>
+
+        <div className='displayTask__task'>
+          <label class='displayTask__task--label'>
             <input type='checkbox' />
             <span className='displayTask__task--text'>
               Pick children at school
             </span>
-          </div>
+          </label>
 
           <HighlightOffIcon />
-        </label>
-        <label class='displayTask__task'>
-          <div>
-            <input type='checkbox' />
-            <span className='displayTask__task--text'>
-              Pick children at school
-            </span>
-          </div>
-
-          <HighlightOffIcon />
-        </label>
-        <label class='displayTask__task'>
-          <div>
-            <input type='checkbox' />
-            <span className='displayTask__task--text'>
-              Pick children at school
-            </span>
-          </div>
-
-          <HighlightOffIcon />
-        </label>
+        </div>
       </div>
     </div>
   );
